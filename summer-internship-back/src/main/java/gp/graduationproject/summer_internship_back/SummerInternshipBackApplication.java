a029f111a2650e6c3fd7dc5e46eec307b1868210
UMO <celik.umutcan@outlook.com>
2025-05-18 16:08:38 +0300

    Staj başvuru e-posta gönderim mantığı düzeltildi ve apply endpoint’inde controller ile service arasındaki uyumsuzluk giderildi.

--- summer-internship-back/src/main/java/gp/graduationproject/summer_internship_back/SummerInternshipBackApplication.java
+++ summer-internship-back/src/main/java/gp/graduationproject/summer_internship_back/SummerInternshipBackApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class SummerInternshipBackApplication implements CommandLineRunner {
 
 	@Autowired
